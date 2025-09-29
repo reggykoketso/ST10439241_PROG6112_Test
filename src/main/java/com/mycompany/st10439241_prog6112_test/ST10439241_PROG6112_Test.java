12c1ad523ff71c9d2f49a81c3ea91b2b24fc4d09
reggykoketso <reggykoketso@gmail.com>
2025-09-29 12:19:11 +0000

    Initial commit

---
+++ src/main/java/com/mycompany/st10439241_prog6112_test/ST10439241_PROG6112_Test.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.st10439241_prog6112_test;
+
+/**
+ *
+ * @author RC_Student_Lab
+ */
+public class ST10439241_PROG6112_Test {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
